e4d8eda7a2fda2c770ce58837cae1afc99aeab76
wangye <im.euii@gmail.com>
2019-10-09 22:32:22 +0800

    修正后没有测试，因为环境已经安装好了

--- snell_install.sh
+++ snell_install.sh
@@ -5,6 +5,10 @@ unzip_file='snell-server'
 bin_dic='/usr/local/bin'
 
 create_systemd_service() {
+
+ if [[ ! -f '/etc/snell-server.conf' && -f '/usr/local/bin/snell-server.conf' ]]; then
+    ln -s /usr/local/bin/snell-server.conf /etc/snell-server.conf
+ fi
  cd /etc/systemd/system
  wget -O snell.service ${snell_systemd_conf}
  systemctl enable snell.service
@@ -17,15 +21,16 @@ if [[ -f ${snell_file_name} ]]; then
   unzip ${snell_file_name}
   mv -f $PWD/${unzip_file} ${bin_dic}
 
-  wait
   if [[ -f '/usr/local/bin/snell-server.conf' ]]; then
-    ln -s /usr/local/bin/snell-server.conf /etc/snell-server.conf
+
     create_systemd_service
-    cat /etc/snell-server.conf;
+    cat /etc/snell-server.conf
   else
     echo 'snell conf is not exist, Initializing...'
     cd ${bin_dic}
     ${bin_dic}/${unzip_file}
+    wait
+    create_systemd_service
   fi
 else
   echo 'file is not exist.'
